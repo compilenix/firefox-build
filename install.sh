4031a470c3cd90d665a258bd416f560315af9008
CompileNix <compilenix@gmail.com>
2024-10-15 22:04:11 +0200

    update install script: add more stages, that can be skipped

--- install.sh
+++ install.sh
@@ -40,45 +40,68 @@ fi
 
 # backup current firefox profiles
 function ask_yn_y_callback() {
+    set -x
     target_dir="/tmp/$UID/.mozilla/$current_firefox_version"
     if [ -d "$target_dir" ]; then
         rm -rf "$target_dir"
     fi
     mkdir -pv "$target_dir"
     rsync -rav "$HOME/.mozilla/firefox" "$target_dir"
+    set +x
     echo "firefox profiles backup location: \"$target_dir\""
 }
-
 if [ -d "$HOME/.mozilla/firefox" ]; then
     echo "backup firefox profiles?"
     ask_yn
     reset-ask_yn
 fi
 
-set -x
-mkdir -pv ~/bin
-cp -v "dist/${version}.tar.bz2" ~/bin/
-cd ~/bin
-if [ -d "${version}" ]; then
-    rm -rf "${version}"
+# extract firefox
+function ask_yn_y_callback() {
+    set -x
+    mkdir -pv ~/bin
+    cp -v "dist/${version}.tar.bz2" ~/bin/
+    cd ~/bin
+    if [ -d "${version}" ]; then
+        rm -rf "${version}"
+    fi
+    mkdir -pv "${version}"
+    tar -xaf "${version}.tar.bz2" --directory "${install_dir}/${version}"
+    set +x
+}
+if [ -d "$HOME/.mozilla/firefox" ]; then
+    echo "extract firefox $version into \"$install_dir\"?"
+    ask_yn
+    reset-ask_yn
 fi
-mkdir -pv "${version}"
-tar -xaf "${version}.tar.bz2" --directory "${install_dir}/${version}"
-ln -sfv "${install_dir}/${version}/firefox/firefox" ./
-cp -v "${install_dir}/${version}/firefox/browser/chrome/icons/default/default128.png" "firefox.png"
-ls -lah --color=auto "${install_dir}/firefox"
+
+# link firefox
+function ask_yn_y_callback() {
+    set -x
+    cd ~/bin
+    ln -sfv "${install_dir}/${version}/firefox/firefox" ./
+    cp -v "${install_dir}/${version}/firefox/browser/chrome/icons/default/default128.png" "firefox.png"
+    ls -lah --color=auto "${install_dir}/firefox"
+    set +x
+}
+if [ -d "$HOME/.mozilla/firefox" ]; then
+    echo "link firefox into ~/bin?"
+    ask_yn
+    reset-ask_yn
+fi
 
 # install firefox desktop file into ~/.local/share/applications
 function ask_yn_y_callback() {
+    set -x
     mkdir -pv "$HOME/bin"
     cp -v "./firefox.desktop" "/tmp/firefox.desktop.tmpl"
     envsubst < "/tmp/firefox.desktop.tmpl" > "/tmp/firefox.desktop"
     desktop-file-install --dir="$HOME/.local/share/applications" --rebuild-mime-info-cache "/tmp/firefox.desktop"
     rm -v "/tmp/firefox.desktop.tmpl" "/tmp/firefox.desktop"
+    set +x
 }
 set +x
 echo "install firefox desktop file into ~/.local/share/applications?"
 ask_yn
-set -x
 reset-ask_yn
 
--- src/build.sh
+++ src/build.sh
@@ -6,5 +6,5 @@ set -x
 env | sort
 nice -n 15 ./mach build
 ./mach package
-cp -fv obj-firefox/dist/firefox-*.tar.bz2 /dist/
+rsync -a obj-firefox/dist/firefox-*.tar.bz2 /dist/
 chmod 0666 /dist/*
